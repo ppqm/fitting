d80e8ef71d2847172fde635c742b0d4f69ecd0c4
Jimmy Charnley Kromann <jimmy@charnley.dk>
2017-06-10 10:50:11 +0200

    added shell function

--- fitter/bin/fit.py
+++ fitter/bin/fit.py
@@ -39,6 +39,21 @@ import time
 import seaborn as sns
 import pandas as pd
 
+from subprocess import Popen, PIPE
+
+
+def shell(cmd, shell=False):
+
+    if shell:
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    else:
+        cmd = cmd.split()
+        p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    output, err = p.communicate()
+    return output
+
+
 output1 = []
 output2 = []
 output3 = []
